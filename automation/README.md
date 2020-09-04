deb7620cb441b1d536c7aad17b0ca00061c05976
Evgeni Golov <evgeni@golov.de>
2020-09-04 10:31:09 +0200

    add package automation scripts and templates

---
+++ automation/README.md
@@ -0,0 +1,35 @@
+# pulpcore RPMs
+
+This repository contains scripts and container files that can be used to add and update RPM packages for pulpcore to pulpcore-packaging.
+
+The container is designed to be run against an empty directory, and produce a set of commits against `pulpcore-packaging` containing the generated packages.
+
+## Dependencies
+
+* podman, docker, or some other container runtime
+
+## Configure
+
+For the tools to work properly, you'll need to provide the container with your name and email address. The simplest way is to create a `packaging.env` file like this:
+```
+NAME=John Doe
+EMAIL=john@example.org
+```
+
+The file can be later passed to the container runtime with `--env-file`. You can of course also use `--env` to specify the values of `NAME` and `EMAIL` directly.
+
+## Execution
+
+### Build
+
+```
+podman build --tag tfm-pulpcore-builder .
+```
+
+### Run
+
+1. create an empty directory to host the resulting packages
+2. pass the directory as a volume for `/app/packaging` to the container:
+```
+podman run --rm --volume $(pwd)/packaging/:/app/packaging/:Z  --env-file packaging.env tfm-pulpcore-builder
+```
---
+++ automation/_generate_deps.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+PULPCORE_REQUIREMENTS="/app/requirements.txt"
+PULPCORE_FULL_REQUIREMENTS="/app/pulpcore-requirements.txt"
+PACKAGING_WORKDIR="/app/packaging"
+FOREMAN_PACKAGING="${PACKAGING_WORKDIR}/foreman"
+FOREMAN_PACKAGING_GIT="https://github.com/theforeman/foreman-packaging"
+FOREMAN_PACKAGING_BRANCH="rpm/develop"
+PULPCORE_PACKAGING="${PACKAGING_WORKDIR}/pulpcore"
+PULPCORE_PACKAGING_GIT="https://github.com/theforeman/pulpcore-packaging"
+PULPCORE_PACKAGING_BRANCH="rpm/3.6"
+PULPCORE_TAG="katello-pulpcore-nightly-el7"
+
+pip3 install scikit-build
+pip3 install -r $PULPCORE_REQUIREMENTS
+pip3 freeze |sed '/gobject/d; /scikit/d; /libcomps/d; /solv/d; /createrepo/d; /distro/d; /^ansible/d' > $PULPCORE_FULL_REQUIREMENTS
+
+pip3 install git+https://github.com/evgeni/pyp2rpm.git@foreman#egg=pyp2rpm
+
+if [ -d $PACKAGING_WORKDIR ]; then
+
+  git clone --branch $FOREMAN_PACKAGING_BRANCH $FOREMAN_PACKAGING_GIT $FOREMAN_PACKAGING
+  git clone --branch $PULPCORE_PACKAGING_BRANCH $PULPCORE_PACKAGING_GIT $PULPCORE_PACKAGING
+
+  pushd $PULPCORE_PACKAGING
+
+  git config --local user.email "${EMAIL:-root@localhost}"
+  git config --local user.name "${NAME:-root}"
+  export RPM_PACKAGER="${NAME}"
+
+  while read line; do
+    pkg=${line%==*}
+    version=${line#*==}
+    REWRITE_ON_SAME_VERSION=false $FOREMAN_PACKAGING/add_pypi_package.sh ${pkg} ${version} $PULPCORE_TAG '/' $PULPCORE_PACKAGING/pyp2rpm/pulpcore.spec
+    # don't hit pypi.org too hard, it sometimes decides to ratelimit us
+    sleep 5
+  done < $PULPCORE_FULL_REQUIREMENTS
+
+  popd
+fi
