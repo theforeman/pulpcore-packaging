50deddde0f306ff6c477a13e54bcdd538ab4df3f
Odilon Sousa <osousa@redhat.com>
2025-02-12 12:01:53 -0300

    Add build system packages to manage updates

---
+++ add_buildsystem_packages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "poetry" >> automation/requirements.txt
+echo "hatch" >> automation/requirements.txt
+echo "flit" >> automation/requirements.txt
+echo "maturin" >> automation/requirements.txt
