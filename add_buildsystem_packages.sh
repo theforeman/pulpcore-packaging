931d1dbc5bba2847c877783825cd80915a02ff83
Odilon Sousa <osousa@redhat.com>
2025-10-02 08:39:13 -0300

    Relax Poetry requirements to get latest version

--- add_buildsystem_packages.sh
+++ add_buildsystem_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "poetry==1.8.3" >> automation/requirements.txt
+echo "poetry" >> automation/requirements.txt
 echo "hatch==1.9.7" >> automation/requirements.txt
 echo "flit" >> automation/requirements.txt
 echo "maturin" >> automation/requirements.txt
