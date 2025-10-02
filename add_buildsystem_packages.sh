fd378498be26656d106a63c492d42baf62a4d7bb
Odilon Sousa <osousa@redhat.com>
2025-10-02 08:57:35 -0300

    Relax hatch versioning to update buildroot

--- add_buildsystem_packages.sh
+++ add_buildsystem_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "poetry" >> automation/requirements.txt
-echo "hatch==1.9.7" >> automation/requirements.txt
+echo "hatch" >> automation/requirements.txt
 echo "flit" >> automation/requirements.txt
 echo "maturin" >> automation/requirements.txt
